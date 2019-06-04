4af01a38901d5aaeb9d53f21a14f2850c4dbbae7
Lauren Eggers <neyabenz@gmail.com>
2019-06-04 14:34:00 -0400

    css

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/img'));
+
 app.get('/', (req, res) => {
     res.render('home.ejs');
 });
