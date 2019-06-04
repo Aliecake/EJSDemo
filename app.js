42de4278eba919963fc13ad2c850dc7f96295d6b
Lauren Eggers <neyabenz@gmail.com>
2019-06-04 14:08:35 -0400

    posts added

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ app.get('/', (req, res) => {
     res.render('home.ejs');
 });
 
+app.get('/posts/', (req, res) => {
+    const posts = [
+        {title: 'Death List', author: 'Arya'},
+        {title: 'Queen of the North', author: 'Sansa'},
+        {title: 'How to warg for no reason', author: 'Bran'}
+    ];
+    res.render('posts.ejs', {posts});
+});
+
 app.get('/fallinlovewith/:thing', (req, res) =>{
     let thing = req.params.thing;
     res.render('love.ejs', {thing});
 });
+
 //404 not found, last get
 app.get('*', (req, res) => {
     res.render('404.ejs');
