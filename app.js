1b4eaaf3b96c2554b9624ee0c52447a7434dd40a
Lauren Eggers <neyabenz@gmail.com>
2019-06-04 13:20:05 -0400

    fun with ejs

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 app.get('/', (req, res) => {
     res.render('home.ejs');
 });
+
+app.get('/fallinlovewith/:thing', (req, res) =>{
+    let thing = req.params.thing;
+    res.render('love.ejs', {thing});
+});
 //404 not found, last get
 app.get('*', (req, res) => {
     res.render('404.ejs');
