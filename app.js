7c85ad6aec5aa09d623002ca4925e2ab95e2ffa7
Lauren Eggers <neyabenz@gmail.com>
2019-06-04 12:39:01 -0400

    setup for express

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-import express from 'express';
+const express = require('express');
 const app = express();
+
+app.get('*', (req, res) => {
+    res.send(`404, we're totally firing that new junior dev`);
+});
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+});
