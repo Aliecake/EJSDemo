ffd1ccecdba2a7ad9b191c7102d3851d915d6acf
Lauren Eggers <neyabenz@gmail.com>
2019-06-05 11:43:33 -0400

    body parser added

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/img'));
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.post('/addenemy', (req, res) => {
+    console.log(req.body)
+    res.send('You have reached the post!');
 });
 
 app.get('/posts/', (req, res) => {
